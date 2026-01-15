46ceaeb837d5a8feb237e25430cc155f72825464
Billy Arante <arantebillywilson@gmail.com>
2026-01-15 22:31:25 +0800

    feat: ✨ add the shots group

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     #
     # Groups
