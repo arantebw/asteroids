2fb1b0b5be552d2e5e321fbcb588e2ac6a682f58
Billy Arante <arantebillywilson@gmail.com>
2026-01-16 22:59:47 +0800

    feat: ✨ split those big asteroids

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,9 @@
 import pygame
+import random
+
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import ASTEROID_MIN_RADIUS, LINE_WIDTH
+from logger import log_event
 
 
 class Asteroid(CircleShape):
@@ -14,3 +17,22 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position = self.position + (self.velocity * dt)
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            log_event("asteroid_split")
+
+            new_vector1 = self.velocity.rotate(random.uniform(20, 50))
+            new_vector2 = self.velocity.rotate(random.uniform(20, 50) * -1)
+
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+            new_asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+            new_asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
+
+            new_asteroid1.velocity = new_vector1 * 1.2
+            new_asteroid2.velocity = new_vector2 * 1.2
+
